a8d20c7e107e4145ffa557a88fcc19978b99b06a
michael febrianto <michaelfebrianto@gmail.com>
2020-01-30 10:49:17 +1100

    update : green build

--- app/src/androidTest/java/com/example/mock/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/mock/ExampleInstrumentedTest.kt
@@ -1,24 +1,50 @@
 package com.example.mock
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.rule.ActivityTestRule
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.core.StringContains
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.IOException
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private val mockWebServer = MockWebServer()
+
+    @Rule @JvmField var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    @Throws(IOException::class, InterruptedException::class)
+    fun setup() {
+        mockWebServer.enqueue(MockResponse().setBody("Wed, 29 Jan 2020 23:19:51 +1000"))
+        mockWebServer.start(8080)
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.mock", appContext.packageName)
+    fun callAPI() {
+        onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click())
+
+        //todo: sleep can avoided with many different way like timeout
+        Thread.sleep(5000)
+
+        onView(ViewMatchers.withId(R.id.hello_world))
+            .check(ViewAssertions.matches(ViewMatchers.withText(StringContains.containsString("+1000"))))
+
     }
 }
--- app/src/main/java/com/example/mock/MainActivity.kt
+++ app/src/main/java/com/example/mock/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.example.mock
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        disableStrictMode()
+        setListener()
+    }
+
+    private fun disableStrictMode() {
+        //WARNING: do not do this on real application
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+    }
+
+    private fun setListener() {
+        button.setOnClickListener { hello_world.text = callAPI() }
+    }
+
+    private fun callAPI(): String {
+        var reader: BufferedReader? = null
+        var response = ""
+
+        try {
+            val myUrl = URL(BuildConfig.SERVER_URL)
+            val conn: HttpURLConnection = myUrl.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
+            conn.setRequestProperty("Content-Type", "application/json")
+            conn.connect()
+            val inputStream: InputStream = conn.getInputStream()
+            reader = BufferedReader(InputStreamReader(inputStream))
+            response = reader.readLine()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        return response
     }
 }
